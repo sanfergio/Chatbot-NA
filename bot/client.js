f59f95e5ce0f2ffa0e4fc0b6bf7c1c9adb5e4ee6
Giovani Sanfer <giovani.marlonfer@gmail.com>
2025-06-24 17:52:24 -0300

    add chatbot

--- bot/client.js
+++ bot/client.js
@@ -4,7 +4,10 @@
 const { Client } = require("whatsapp-web.js");
 
 // Cria o cliente (como se fosse abrir o WhatsApp Web)
-const client = new Client();
-
+const client = new Client({
+  puppeteer: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  }
+});
 // Exporta o cliente para que outros arquivos possam usá-lo
 module.exports = { client };
